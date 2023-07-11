bc185de5765b9b8edaecf483cdc33e5c1d5a7427
Liza176 <138390166+Liza176@users.noreply.github.com>
2023-07-11 21:43:20 +0600

    Update hm6.3.py

--- hm6.3.py
+++ hm6.3.py
@@ -5,9 +5,10 @@ max2 = -99999999999999999999999999999999999
 for i in range(n):
     n2 = int(input())
     if n2 > max1 :
+        max2 = max1
         max1 = n2
-    
-                                                    
-
-
+    elif n2 > max2 and n2 != max1:
+        max2 = n2
+        
 print(max1)
+print(max2)
