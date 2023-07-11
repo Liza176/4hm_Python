19db1ea3b201ff0a861632e7845e021d075610cd
Liza176 <138390166+Liza176@users.noreply.github.com>
2023-07-11 20:55:26 +0600

    Add files via upload

---
+++ hm1.4.py
@@ -0,0 +1,8 @@
+stroka = print('Введите цифры ')
+count = 0
+n2 = 1
+while n2 % 2 != 0:
+    n2 = int(input()) 
+    count += 1
+
+print(count)
---
+++ hm7.3.py
@@ -0,0 +1,12 @@
+n = int(input('Напишите сколько раз '))
+rezult = 0
+
+for i in range(n):
+    n2 = int(input())
+  
+if n2 % 2 == 0 :
+    print('YES')
+else:
+    print('NO')
+
+
